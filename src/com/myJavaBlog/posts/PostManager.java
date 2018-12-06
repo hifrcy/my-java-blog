3add5f2bb39e4140eda15d6e5f31fb790346877a
Romain Guillemot <romain.guillemot@wildcodeschool.fr>
2018-12-06 11:43:36 +0100

    Post manager : added constructor

--- src/com/myJavaBlog/posts/PostManager.java
+++ src/com/myJavaBlog/posts/PostManager.java
@@ -4,4 +4,14 @@ import java.util.ArrayList;
 
 public class PostManager {
 	private ArrayList<Post> posts;
+	
+	public PostManager() {
+		this.posts = new ArrayList<Post>();
+		this.posts.add(
+			new Post(1, "lorem ipsum", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+		);
+		this.posts.add(
+			new Post(2, "elsass ipsum", "Lorem Elsass ipsum Wurschtsalad DNA, geïz commodo placerat.")
+		);
+	}
 }
