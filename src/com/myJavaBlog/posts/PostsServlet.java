435e5d646234adb058657ded569c81931b3fd697
Romain Guillemot <romain.guillemot@wildcodeschool.fr>
2018-12-06 09:49:13 +0100

    GET:/posts -> created servlet

---
+++ src/com/myJavaBlog/posts/PostsServlet.java
@@ -0,0 +1,33 @@
+package com.myJavaBlog.posts;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PostsServlet
+ */
+@WebServlet("/posts")
+public class PostsServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PostsServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
