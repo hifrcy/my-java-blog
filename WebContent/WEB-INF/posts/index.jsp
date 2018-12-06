bab5ee32559d2b06916f7fa833fcce91d224d0e2
Romain Guillemot <romain.guillemot@wildcodeschool.fr>
2018-12-06 11:40:28 +0100

    GET:/posts -> forwarded a list of post from servlet to jsp

--- WebContent/WEB-INF/posts/index.jsp
+++ WebContent/WEB-INF/posts/index.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ page import="java.util.List" %>
 <%@ page import="com.myJavaBlog.posts.Post" %>
 <!DOCTYPE html>
 <html>
@@ -10,8 +11,9 @@
 <body>
 <h1>My Posts</h1>
 <ul>
-	<% Post post = (Post) request.getAttribute("post"); %>
-	<li><a href="?id=<%= post.getId() %>"><%= post.getTitle() %></a></li>
+	<% for(Post post : (List<Post>)request.getAttribute("posts")) { %>
+		<li><a href="?id=<%= post.getId() %>"><%= post.getTitle() %></a></li>
+	<% } %>
 </ul>
 </body>
 </html>
--- src/com/myJavaBlog/posts/PostsServlet.java
+++ src/com/myJavaBlog/posts/PostsServlet.java
@@ -1,6 +1,8 @@
 package com.myJavaBlog.posts;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,8 +28,14 @@ public class PostsServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		var post = new Post(1, "lorem ipsum", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
-		request.setAttribute("post", post);
+		var posts = new ArrayList<Post>();
+		posts.add(
+			new Post(1, "lorem ipsum", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+		);
+		posts.add(
+			new Post(2, "elsass ipsum", "Lorem Elsass ipsum Wurschtsalad DNA, geïz commodo placerat.")
+		);
+		request.setAttribute("posts", posts);
 		
 		this.getServletContext().getRequestDispatcher("/WEB-INF/posts/index.jsp").forward(request, response);
 	}
