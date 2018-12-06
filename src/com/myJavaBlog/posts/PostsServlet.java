5884a120bc2cfe6b25f5b049b35ade5159f8c3dc
Romain Guillemot <romain.guillemot@wildcodeschool.fr>
2018-12-06 09:51:11 +0100

    GET:/posts -> forwarded request from servlet to jsp

--- src/com/myJavaBlog/posts/PostsServlet.java
+++ src/com/myJavaBlog/posts/PostsServlet.java
@@ -27,7 +27,7 @@ public class PostsServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		this.getServletContext().getRequestDispatcher("/WEB-INF/posts/index.jsp").forward(request, response);
 	}
 
 }
