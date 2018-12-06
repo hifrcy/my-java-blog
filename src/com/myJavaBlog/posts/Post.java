012af01021c3da4a38fecc7cb77e894010b002d6
Romain Guillemot <romain.guillemot@wildcodeschool.fr>
2018-12-06 09:56:22 +0100

    Post entity -> added constructor

--- src/com/myJavaBlog/posts/Post.java
+++ src/com/myJavaBlog/posts/Post.java
@@ -4,4 +4,10 @@ public class Post {
 	private int id;
 	private String title;
 	private String content;
+	
+	public Post(int id, String title, String content) {
+		this.id = id;
+		this.title = title;
+		this.content = content;
+	}
 }
