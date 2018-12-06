991854914d481c4ba73070a7e8cb2dd46959c21d
Romain Guillemot <romain.guillemot@wildcodeschool.fr>
2018-12-06 09:57:58 +0100

    Post entity -> added getters and setters

--- src/com/myJavaBlog/posts/Post.java
+++ src/com/myJavaBlog/posts/Post.java
@@ -10,4 +10,28 @@ public class Post {
 		this.title = title;
 		this.content = content;
 	}
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getTitle() {
+		return this.title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getContent() {
+		return this.content;
+	}
+	
+	public void setContent(String content) {
+		this.content = content;
+	}
 }
